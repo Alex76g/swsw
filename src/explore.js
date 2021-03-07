15358b6f58d7e99df22ab6cf935b80eaad73a855
Alex <galexics@chemaxon.com>
2021-03-07 13:59:31 +0100

    exploring with remote procedure call

--- src/explore.js
+++ src/explore.js
@@ -1,4 +1,9 @@
-console.log(params);
+console.log("belül:", params);
+
+params.egyik = params.egyik || "egyik placeholder";
+params.masik = params.masik || "masik placeholder";
+
+params.harmadik = params.harmadik || "harmadik placeholder";
 
 //function remoteFn(){
 //    console.log("invoked");
--- src/invoke_param.js
+++ src/invoke_param.js
@@ -1,12 +1,28 @@
-var passedParam = "ezt a szöveget én írtam hozzá"
+// This code snippet invokes another js code and even passes (returns?) parameters
+var egySzoveg = "ez egy szöveg";
+var masikSzoveg = "ez meg egy másik";
+var ft = 42;
+
+var complexData = {
+   egyik: egySzoveg,
+   masik: masikSzoveg,
+   inline: "inline szöveg",
+   answer: ft
+};
+
+console.log("előtte:", complexData);
+
 // This code-snippet invokes another js code
 fetch("modules/swsw/src/explore.js")
   .then(function(response) {
     return response.text();
-  }).then(function(data) {
-//    console.log(data); // this will be a string
-    new Function(data)();
+  }).then(function(loadedFunctionBody) {
+//    console.log(loadedFunctionBody); // this will be a string
+
+//    var myFunc =  new Function("params", loadedFunctionBody);
+//    myFunc(complexData);
+
+    new Function("params", loadedFunctionBody)(complexData);
 });
 
-remoteFn();
-remoteFn();
+console.log("utána:", complexData);
