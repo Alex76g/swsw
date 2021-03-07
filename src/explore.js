362bc1854208f6568fa6133142b385e506006091
Alex <galexics@chemaxon.com>
2021-03-07 14:08:04 +0100

    exploring with remote procedure call

--- src/explore.js
+++ src/explore.js
@@ -1,9 +1,11 @@
 console.log("belül:", params);
+console.log("belül a harmadik:", params.harmadik);
 
 params.egyik = params.egyik || "egyik placeholder";
 params.masik = params.masik || "masik placeholder";
 
 params.harmadik = params.harmadik || "harmadik placeholder";
+console.log("belül a harmadik a végén:", params.harmadik);
 
 //function remoteFn(){
 //    console.log("invoked");
