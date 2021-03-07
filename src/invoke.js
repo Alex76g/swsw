6668a5f056c1cb9c592fc50f9cc9d05d2e776607
Alex <galexics@chemaxon.com>
2021-03-07 17:14:55 +0100

    exploring with remote procedure call

--- src/invoke.js
+++
@@ -1,8 +0,0 @@
-// This code-snippet invokes another js code (without any context)
-fetch("modules/swsw/src/attack.js")
-  .then(function(response) {
-    return response.text();
-  }).then(function(data) {
-//    console.log(data); // this will be a string
-    new Function(data)();
-});
--- src/invoke_2inside.js
+++ src/invoke_2inside.js
@@ -1,9 +1,9 @@
 console.log("belül:", params);
 
 var result = {
-   egyik = params.egyik || "egyik placeholder",
-   masik = params.masik || "masik placeholder",
-   harmadik = params.harmadik || "harmadik placeholder"
+   egyik: params.egyik || "egyik placeholder",
+   masik: params.masik || "masik placeholder",
+   harmadik: params.harmadik || "harmadik placeholder"
 };
 
 console.log("összerakott:", result);
