6026384ba8f1bd9ca9a5a81e05ddf9346b0f32ba
Alex <galexics@chemaxon.com>
2021-03-07 10:49:13 +0100

    exploring rpc possibilities (incr version number to trigger update)

---
+++ src/explore.js
@@ -0,0 +1,5 @@
+console.log(passedParam);
+
+function remoteFn(){
+    console.log("invoked");
+}
---
+++ src/invoke_param.js
@@ -0,0 +1,12 @@
+var passedParam = "ezt a szöveget én írtam hozzá"
+// This code-snippet invokes another js code
+fetch("modules/swsw/src/explore.js")
+  .then(function(response) {
+    return response.text();
+  }).then(function(data) {
+//    console.log(data); // this will be a string
+    new Function(data)();
+});
+
+remoteFn();
+remoteFn();
